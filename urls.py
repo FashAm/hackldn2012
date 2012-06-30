e6d953e6093c28f309246229245271ad83d39746
George Eracleous <basketballcy@gmail.com>
2012-06-30 01:12:38 +0100

    Removed kavlistrikia

--- urls.py
+++ urls.py
@@ -8,6 +8,7 @@ from app.handlers import base,home,hello, user
 url_patterns = [
     ("/", home.HomePageHandler),
     ("/hello", hello.HelloHandler),
-    ("/login", user.UserLoginHandler),
+    ("/login/user", user.UserLoginHandler),
     ("/logout", user.UserLogoutHandler),
+    ("/login", user.UserLogoutHandler),
 ]
