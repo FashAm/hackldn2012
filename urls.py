3d66e4868292bdb20a4af7bd50097683623c3c1f
George Eracleous <basketballcy@gmail.com>
2012-06-30 05:07:47 +0100

    Cookies

--- urls.py
+++ urls.py
@@ -8,7 +8,6 @@ from app.handlers import base,home,hello, user
 url_patterns = [
     ("/", home.HomePageHandler),
     ("/hello", hello.HelloHandler),
-    ("/login/user", user.UserLoginHandler),
-    ("/logout", user.UserLogoutHandler),
-    ("/login", user.UserLogoutHandler),
+    ("/login/submit", user.UserLoginHandler),
+    ("/login", user.UserLoginOptionsHandler),
 ]
