7bd97d0d0234cda218a6dc5663a732866a96d8c5
George Eracleous <basketballcy@gmail.com>
2012-06-30 21:56:50 +0100

    Reeiving info from appo

--- app/handlers/post.py
+++ app/handlers/post.py
@@ -33,39 +33,49 @@ class PostRequestHandler(base.BaseHandler):
 
     def on_post(self):
         try:
-            img = self.store_images() 
-            ident = self.request.arguments['userId']
+            imgs = []
+            for i in range(int(self.request.arguments['numPhotos'][0])):
+                img_name = 'myImage' + str(i) 
+                imgs.append(self.request.arguments[img_name][0])
+            images = self.store_images(imgs) 
+            
             post = Post()
-            post.aid = ident[0]
-            post.desc = "POutses MplE OuOuuuOuOu"
-            post.visibility = ["xrends", "special"]
-            post.images.append(img)
+            post.aid = self.request.arguments['userId'][0]
+            post.desc = self.request.arguments['description'][0]
+            post.tags = self.request.arguments['tag']
+            post.visibility = self.request.arguments['visibility']
+            
+            for image in images:
+                post.images.append(image)
+
             post.save()
             self.write('Your photo was successfully fashamified. Very soon other stylish Fashamers will give feedback.')
         except Exception,e :
             print "Error"+str(e)
 
-    def store_images(self):
+    def store_images(self, images):
         '''
         Stores images in the db.
         '''
         try:
-            newjpgtxt = self.request.arguments['myImage'][0]
-            raw_image = {}
-            raw_image['body'] = base64.decodestring(newjpgtxt)
-            id = str(uuid.uuid4())
-            raw_image['filename'] = "image_"+id+".jpeg"
-            image = PostImage()
-            size1 = ImageSize()
-            size1.size_x = 1200
-            size1.size_y = 2048
-            size2 = ImageSize()
-            size2.size_x = 300
-            size2.size_y = 512
-            image.sizes = list([size1, size2])
-            url = image.store(raw_image, id)
-            image.url = url
-            #image.save()
-            return image
+            imgs =[]
+            for image in images:
+                newjpgtxt = image
+                raw_image = {}
+                raw_image['body'] = base64.decodestring(newjpgtxt)
+                id = str(uuid.uuid4())
+                raw_image['filename'] = "image_"+id+".jpeg"
+                image = PostImage()
+                size1 = ImageSize()
+                size1.size_x = 1200
+                size1.size_y = 2048
+                size2 = ImageSize()
+                size2.size_x = 300
+                size2.size_y = 512
+                image.sizes = list([size1, size2])
+                url = image.store(raw_image, id)
+                image.url = url
+                imgs.append(image)
+            return imgs
         except Exception, e:
             self.xhr_response = "err,An error occured."
